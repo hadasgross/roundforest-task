3dd93850e843b6d41e67e42a2e488dd7c6154646
hadasgross <hadasgr@gmail.com>
2017-07-10 23:11:39 +0300

    add queries

--- Queries.java
+++ Queries.java
@@ -7,21 +7,19 @@ public class Queries
 {
     // JDBC driver name and database URL
     static final String JDBC_DRIVER = "org.sqlite.JDBC";
-    static final String DB_URL = "jdbc:sqlite:database.sqlite";
+    static final String DB_URL = "jdbc:sqlite:D:/task/src/database.sqlite";
+    static Connection conn = null;
 
     public static void connect()
     {
 
-        Connection conn = null;
-        try{
+        try
+        {
             Class.forName(JDBC_DRIVER);
             conn = DriverManager.getConnection(DB_URL);
             System.out.println("Connecting to database..."); //YEEE that works!
-        } catch(SQLException e)
-        {
-            System.out.println(e.getMessage());
         }
-        catch (java.lang.ClassNotFoundException e)
+        catch(SQLException | ClassNotFoundException e)
         {
             System.out.println(e.getMessage());
         }
@@ -29,11 +27,35 @@ public class Queries
     public static void firstQuery()
     {
         Statement statement = null;
+
+        try
+        {
+            statement = conn.createStatement();
+
+
+            String countNames = "SELECT ProfileName FROM\n(SELECT ProfileName, COUNT(Id)\n" +
+                    "FROM Reviews\nGROUP BY ProfileName\nORDER BY COUNT(Id) DESC) LIMIT 1000";
+            statement.executeQuery(countNames);
+
+
+            statement.close();
+        }
+        catch (SQLException e)
+        {
+            e.printStackTrace();
+        }
     }
 
     public static void main(String[] args)
     {
         connect();
+        firstQuery();
+        try {
+            //DatabaseMetaData md = conn.getMetaData();
+            conn.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
 
